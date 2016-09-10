af91af475729a650a0f0301f015fca0c0768bb9e
Andrew Tattersall <atattersall@hotmail.com>
2016-09-10 09:38:09 -0400

    updated controller

--- client/controllers/mainController.js
+++ client/controllers/mainController.js
@@ -1,6 +1,10 @@
 (function() {
     angular.module('demoApp')
         .controller('mainController', ['$scope', function($scope) {
-            $scope.message = "Angular is Up!"
+            $scope.message = "Angular is Up!";
+            $scope.tab = 0;
+            this.settab = function(tab) {
+                $scope.tab = tab;
+            };
         }]);
 })();
