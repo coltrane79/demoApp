8ae6ee8f5b325a132c1f3ed45d4dbe9a6430ba86
Andrew Tattersall <atattersall@hotmail.com>
2016-09-10 19:59:22 -0400

    create secondary navigation

--- client/app.js
+++ client/app.js
@@ -1,3 +1,18 @@
 (function() {
-    angular.module('demoApp', []);
+    angular.module('demoApp', ['ngRoute'])
+        .config(['$routeProvider', function($routeProvider) {
+            $routeProvider
+                .when('/daily', {
+                    templateUrl: 'views/daily.html'
+                })
+                .when('/people', {
+                    templateUrl: 'views/people.html'
+                })
+                .when('/ar', {
+                    templateUrl: 'views/Customer.html'
+                })
+                .when('/ap', {
+                    templateUrl: 'views/Vendor.html'
+                });
+        }]);
 })();
--- client/controllers/mainController.js
+++ client/controllers/mainController.js
@@ -1,7 +1,7 @@
 (function() {
     angular.module('demoApp')
         .controller('mainController', ['$scope', function($scope) {
-            $scope.message = "Angular is Up!";
+            $scope.appTitle = "Business Manager";
             $scope.tab = 0;
             this.settab = function(tab) {
                 $scope.tab = tab;
